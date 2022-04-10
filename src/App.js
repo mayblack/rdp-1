83c4cd4f14f11107fe2bc337ccabe410e198f497
Marcin Rudzki <mar.rudzki@gmail.com>
2022-04-10 18:40:42 +0200

    use RegularListing to display products

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { RightHandComponent } from './RightHandComponent';
 import {RegularList} from './RegularList';
 import {SmallPersonListItem} from './people/SmallPersonListItem';
 import {LargePersonListItem} from './people/LargePersonListItem';
+import {SmallProductListItem} from './products/SmallProductListItem';
 
 const people = [{
     name: 'John Doe',
@@ -22,6 +23,23 @@ const people = [{
     hobbies: ['biology', 'medicine', 'gymnastics'],
 }];
 
+const products = [{
+    name: 'Flat-Screen TV',
+    price: '$300',
+    description: 'Huge LCD screen, a great deal',
+    rating: 4.5,
+}, {
+    name: 'Basketball',
+    price: '$10',
+    description: 'Just like the pros use',
+    rating: 3.8,
+}, {
+    name: 'Running Shoes',
+    price: '$120',
+    description: 'State-of-the-art technology for optimum running',
+    rating: 4.2,
+}];
+
 function App() {
   return (
       <div>
@@ -33,6 +51,8 @@ function App() {
           <RegularList items={people} resourceName="person" itemComponent={SmallPersonListItem}/>
           <hr/>
           <RegularList items={people} resourceName="person" itemComponent={LargePersonListItem}/>
+          <hr/>
+          <RegularList items={products} resourceName="product" itemComponent={SmallProductListItem}/>
       </div>
   );
 }
